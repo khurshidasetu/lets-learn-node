d36a0a1fa74b42c3891a74be1b42ca7b4440b497
khurshidasetu <setu@orangetoolz.com>
2026-04-30 14:51:31 +0600

    style: apply consistent spacing and update /about route to /about-blah in server.js
    added nodemon

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method,"request made")
+    console.log(req.url, req.method, "request made")
 
     res.setHeader('Content-Type', 'text/html');
 
     let path = './views/';
-    switch(req.url){
+    switch (req.url) {
         case '/':
             path += 'index.html';
             break;
         case '/about-us':
             path += 'about-us.html';
             break;
-        case '/about':
+        case '/about-blah':
             res.statusCode = 301;
             res.setHeader('Location', '/about-us')
             res.end()
@@ -24,7 +24,7 @@ const server = http.createServer((req, res) => {
             break;
     }
 
-    fs.readFile(path,(err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err)
             res.end('<h1>Error</h1>')
@@ -33,6 +33,6 @@ const server = http.createServer((req, res) => {
     })
 })
 
-server.listen(3000,'localhost', () => {
+server.listen(3000, 'localhost', () => {
     console.log('Server is running on port 3000')
 })
