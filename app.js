1b07031058c19eecf59e3d61eca8cf3117a64d0a
khurshidasetu <setu@orangetoolz.com>
2026-04-30 17:00:30 +0600

    feat: add /about-blah redirect and implement 404 and error handling middleware

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ app.get('/about-us', (req, res)=>{
     // res.send('<h1>about us page</h1>');
     res.sendFile('./views/about-us.html',{root:__dirname});
 })
+
+app.get('/about-blah',(req,res)=>{
+    console.log("about blah page",req.url)
+
+    res.redirect('/about-us');
+})
+
+app.use((req,res)=>{
+    console.log("404 page",req.url)
+
+    res.status(404).sendFile('./views/404.html',{root:__dirname});
+})
+
+// error page 
+app.use((err,req,res)=>{
+   console.log("error",err)
+   res.status(404).sendFile('./views/404.html',{root:__dirname})
+});
